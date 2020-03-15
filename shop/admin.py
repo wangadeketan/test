200172ef614a57d1b516012acbeca690c9037bd2
Ketan Wangade <wangadeketan@gmail.com>
2020-03-15 11:36:33 +0530

    added media folder in a gitignore

--- shop/admin.py
+++ shop/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from . models import Category, Product, Customer, OrderItem, Order, Cart
 
 # Register your models here.
+
+class ProductModelAdmin(admin.ModelAdmin):
+	"""Product Model class in a admin"""
+	list_display = ["name","price","date"]
+	list_display_links = ("price","date")
+	#list_filter = ("name")
+	class Meta:
+		model = Product
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductModelAdmin)
 admin.site.register(Customer)
 admin.site.register(OrderItem)
 admin.site.register(Order)
